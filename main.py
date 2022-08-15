fd00f6eda9308574bef75b87d815e50afc0614d4
Arthur Hoarau <arthur.hoarau@outlook.fr>
2022-08-15 08:46:29 +0200

    Adding conflict EDT code

---
+++ main.py
@@ -0,0 +1,26 @@
+from decision_tree_imperfect import EDT
+import numpy as np
+
+"""
+Example with 2 imperfectly labeled observations.
+"""
+
+# Training X values [x1, x2]
+X_train = np.array([[0.5, 0.5],
+                    [-0.5, -0.5]])
+
+# Training y values, basic belief assigments (with or without empty set )
+# [c1, c2, c1 U c2]  or [0, c1, c2, c1 U c2] 
+y_train = np.array([[0.9, 0, 0.1],
+                    [0, 0.9, 0.1]])
+
+# Test dataset with TRUE class (0 or 1)
+X_test = np.array([[0.1, 0.1]])
+y_test = np.array([0])
+
+classifier = EDT()
+classifier.fit(X_train, y_train)
+
+precisions = classifier.score(X_test, y_test)
+
+print("Accuracy : ", precisions)
